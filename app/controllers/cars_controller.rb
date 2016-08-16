de783839d4d9cdabcb9ff9dc482fcacdee1faa8c
Cedric de Radzitzky <cderadzitzky@gmail.com>
2016-08-16 17:57:39 +0200

    Car controller and car views INDEX SHOW

---
+++ app/controllers/cars_controller.rb
@@ -0,0 +1,18 @@
+class CarsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+  before_action :find_car, only: [:show]
+
+  def index
+    @cars = Car.all
+    # cars GET    /cars(.:format)                           cars#index
+  end
+
+  def show
+    # car GET    /cars/:id(.:format)                       cars#show
+  end
+
+  private
+    def find_car
+      @car = Car.find(params[:id])
+    end
+end
