3b66c374d121210ff01c4f30be2d7c329c328634
Cedric de Radzitzky <cderadzitzky@gmail.com>
2016-08-17 13:15:47 +0200

    dashboard car index

--- app/controllers/dashboard/cars_controller.rb
+++ app/controllers/dashboard/cars_controller.rb
@@ -1,3 +1,33 @@
 class Dashboard::CarsController < ApplicationController
 
+def index
+  @cars = current_user.cars
 end
+
+def show #(+route) ?
+end
+
+def new
+  @car = Car.new
+end
+def create
+  @car = Car.new(cocktail_params)
+    if @car.save
+      redirect_to car_path(@car)
+    else
+      render :new
+    end
+end
+
+
+
+
+
+end
+
+       #     dashboard_cars GET    /dashboard/cars(.:format)                 dashboard/cars#index
+       #                    POST   /dashboard/cars(.:format)                 dashboard/cars#create
+       #  new_dashboard_car GET    /dashboard/cars/new(.:format)             dashboard/cars#new
+       # edit_dashboard_car GET    /dashboard/cars/:id/edit(.:format)        dashboard/cars#edit
+       #      dashboard_car PATCH  /dashboard/cars/:id(.:format)             dashboard/cars#update
+       #                    PUT    /dashboard/cars/:id(.:format)             dashboard/cars#update
