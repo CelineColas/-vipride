4aa904af5ddba0a5019451b283e9197b52bea6f6
Yiannis Stouraiti <iostouraitis@gmail.com>
2016-08-19 00:16:43 +0200

    improve css for bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817091928) do
+ActiveRecord::Schema.define(version: 20160818103205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20160817091928) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "photo"
     t.index ["user_id"], name: "index_cars_on_user_id", using: :btree
   end
 
