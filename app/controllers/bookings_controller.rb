e23ad7e08603122a564fb4939d679d1a2a72a5a3
Yiannis Stouraiti <iostouraitis@gmail.com>
2016-08-18 18:08:40 +0200

    work on views and forms of bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,11 +6,13 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @booking = Booking.new(booking_params)
+    @booking = Booking.new(booking_params)  # we pass the params values during the creation of a booking
+    @booking.user = current_user            # we assign the current user to the booking
+    @booking.car_id = params[:car_id]       # we assing the car id to the booking through the params
+    @booking.status = "Pending"             # we assign a default state at the status, which shoul match the validations
     @booking.save!
-    redirect_to dashboard_bookings_path
+    redirect_to dashboard_bookings_path     # we redirect back to the dashboard
   end
-
 
   private
   # def set_booking
@@ -18,6 +20,6 @@ class BookingsController < ApplicationController
   # end
 
   def booking_params
-    params.require(:booking).permit(:message, :status, :start_date, :end_date, :car_id, :user_id)
+    params.require(:booking).permit(:message, :status,:start_date, :end_date, :car_id, :user_id)
   end
 end
